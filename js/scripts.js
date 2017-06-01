dfcd5b578ed4adb93a7ba32255d6806ab72fb298
Alena Golovina and Mick Sexton <mick@>
2017-06-01 11:35:31 -0700

    finish html and js, add bootstrap theme from bootstrap live customizer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  $("#user-info").submit(function() {
+    var nameInput = $("input#full-name").val();
+    var itemInput = $("input#store-item").val();
+    var addressInput = $("input#address").val();
+
+    $(".name").text(nameInput);
+    $(".item").text(itemInput);
+    $(".address").text(addressInput);
+
+     $("#receipt").show();
+
+     event.preventDefault();
+  });
 });
